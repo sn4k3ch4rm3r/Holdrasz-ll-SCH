7957ebdd972a9d9b4a0947ff3809d70f11a8ed7f
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2022-11-01 14:41:37 +0100

    Render lunar lander sprite

--- include/lander.h
+++ include/lander.h
@@ -3,9 +3,10 @@
 
 #include <SDL.h>
 #include "vector.h"
+#include "camera.h"
 
 typedef struct Lander {
-	Vector2 pos;
+	Vector2 position;
 	Vector2 velocity;
 	double rotation;
 	double angular_velocity;
@@ -13,6 +14,6 @@ typedef struct Lander {
 	double propellant;
 } Lander;
 
-void render_lander(SDL_Renderer *renderer, Lander *lander);
+void render_lander(Camera *camera, Lander *lander);
 void lander_total_mass(Lander *lander);
 #endif
--- src/lander.c
+++ src/lander.c
@@ -1,6 +1,20 @@
 #include <SDL.h>
-#include <SDL2_gfxPrimitives.h>
-#include <math.h>
+#include <SDL_image.h>
 
 #include "lander.h"
 #include "vector.h"
+#include "camera.h"
+
+#include "LunarModule.xpm"
+
+void render_lander(Camera *camera, Lander *lander) {
+	const int width = 64;
+	const int height = 64;
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(camera->renderer, IMG_ReadXPMFromArray(lunar_module));
+
+	Vector2 screen_coords = get_screen_coordinates(camera, lander->position);
+	SDL_Rect dst = {screen_coords.x, screen_coords.y, width * camera->zoom, height * camera->zoom};
+
+	SDL_RenderCopyEx(camera->renderer, texture, NULL, &dst, lander->rotation, NULL, SDL_FLIP_NONE);
+	SDL_DestroyTexture(texture);
+}

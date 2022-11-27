7fd6811e29802be9038e47dd3f0a317b6278ea68
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2022-11-27 11:10:39 +0100

    Particles bounce

--- src/particle.c
+++ src/particle.c
@@ -1,10 +1,12 @@
 #include <SDL.h>
 #include <SDL2_gfxPrimitives.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "particle.h"
 #include "vector.h"
 #include "camera.h"
+#include "terrain.h"
 
 #include "debugmalloc.h"
 
@@ -28,6 +30,28 @@ void append_particle(List *list, Particle p) {
 void update_particles(List *list, double dt) {
 	ListElement *element = list->first;
 	while(element != NULL) {
+		double ds = 0.5;
+		Vector2 p1 = {
+			floor(element->particle.position.x), get_terrain_height(floor(element->particle.position.x))
+		};
+		Vector2 p2 = {
+			floor(element->particle.position.x)+ds, get_terrain_height(floor(element->particle.position.x)+ds)
+		};
+
+		double slope = p2.y - p1.y;
+		double angle = (atan(slope/ds) / M_PI) * 180;
+
+		Vector2 force = {0, 0};
+		if(element->particle.position.y < get_terrain_height(element->particle.position.x)) {
+			Vector2 v = V_rotate(element->particle.velocity, -angle);
+			force.y = 0.01 * 2 * (-v.y / dt);
+			force.x = 0.01 * 2 * (-v.x / dt);
+			double impact_angle = tan(force.y / force.x) / M_PI * 180;
+			force = V_rotate(force, angle + 180 - impact_angle);
+		}
+		Vector2 acceleration = V_divide_const(force, .01);
+		element->particle.velocity = V_add(element->particle.velocity, V_multiply_const(acceleration, dt));
+
 		element->particle.position = V_add(element->particle.position, V_multiply_const(element->particle.velocity, dt));
 		element->particle.lived += dt;
 		ListElement *next = element->next;
